dfb3c53ca4d0f9d2e7fa200be679f8183622707d
naoyashiga <naoyashiga0@gmail.com>
2014-09-16 20:54:24 +0900

    Initial Commit

---
+++ KusogeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KusogeTest
+//
+//  Created by naoyashiga on 2014/09/16.
+//  Copyright (c) 2014年 naoyashiga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KusogeTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KusogeTest
+//
+//  Created by naoyashiga on 2014/09/16.
+//  Copyright (c) 2014年 naoyashiga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
